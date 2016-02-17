7ad59b886183913be9c368d4f5b5a95ac5320214
Dominik Heidler <dominik@heidler.eu>
2016-02-17 01:31:57 +0100

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(                                                                                                                                         
 	name='pyalfred',
-	version='0.0.1',
+	version='0.0.2',
 	license='GPL',
 	description='Python client library for alfred mesh monitoring',
 	author='Dominik Heidler',
